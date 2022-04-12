dd2cbc52f47f417214204d539a1353ba6fc5e25e
loicekm <31313103+loicekm@users.noreply.github.com>
2022-04-12 20:35:01 +0200

    my add

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_alphabet - print alphabet lowercase
  *
  * Return: Always 0.
  */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-
 void print_alphabet(void)
 {
 char alphabet;
