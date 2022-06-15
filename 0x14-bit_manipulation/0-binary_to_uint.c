be0156efacbf0f9b0f6b67f311ebc1602772049e
unknown <nyandiakamau24@gmail.com>
2022-06-15 13:52:32 +0200

    this

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,65 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
- */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
-}
-
-/**
- * _strlen - function that returns the length of a string.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * @s: pointer to an string
- * Return: int
+ * Return: the converted number
  */
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i += 1;
-	}
-	return (i);
-}
-
-/**
- * binary_to_uint - function that converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	int len, exp = 0;
-	unsigned int res = 0;
+    int i;
+    unsigned int dec_val = 0;
 
-	if (b == NULL)
-		return (0);
-	len = _strlen(b);
-	while (len-- && len >= 0)
-	{
-		if (b[len] == '1')
-			res += _pow_recursion(2, exp);
-		else if (b[len] != '0')
-			return (0);
-		exp++;
-	}
-	return (res);
+    if (!b)
+        return (0);
+
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
+
+    return (dec_val);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,49 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
-}
-
-/**
- * print_binary - function that prints the binary representation of a number
- * @n: decimal number
- * Return: nothing
- */
-
 void print_binary(unsigned long int n)
 {
-	unsigned int res_pow = 0;
-	int exp = 10;
-	int flag = 0;
+    int i, count = 0;
+    unsigned long int current;
 
-	if (n == 0)
-		_putchar('0');
-	while (exp >= 0)
-	{
-		res_pow = _pow_recursion(2, exp);
-		if (n >= res_pow)
-		{
-			_putchar('1');
-			n -= res_pow;
-			flag = 1;
-		}
-		else if (n < res_pow && flag == 1)
-			_putchar('0');
-		exp--;
-	}
+    for (i = 63; i >= 0; i--)
+    {
+        current = n >> i;
+
+        if (current & 1)
+        {
+            _putchar('1');
+            count++;
+        }
+        else if (count)
+            _putchar('0');
+    }
+    if (!count)
+        _putchar('0');
 }
